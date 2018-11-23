968dfba84db08152c030de5c7f448928be0d0315
sunmingqian <1974881240@qq.com>
2018-11-23 15:19:40 +0800

    brand页

--- src/components/brand.vue
+++ src/components/brand.vue
@@ -31,20 +31,91 @@
 	    </div>
 
 	    <div class="brandproducts">
-	    	<div class=""></div>
+	    	<div class="swiper-container">
+	    		<h2>精选上新</h2>
+			    <div class="swiper-wrapper">
+			        <div class="swiper-slide" v-for="data in produncts">
+			        	
+			        		<img :src="data.fileUrl">
+			        		<div class="imgmask"></div>
+			        		<div class="price">
+			        			<span>￥{{data.price}}</span>
+			        			<span>￥{{data.marketPrice}}</span>
+			        		</div>
+			        	
+			        </div> 
+			    </div>
+
+			   
+			</div>
 	    </div>
+
+	    <div class="hotproducts">
+	    	<div class="swiper-container">
+	    		<h2>人气热销</h2>
+			    <div class="swiper-wrapper">
+			        <div class="swiper-slide" v-for="data in hot">
+			        		<img :src="data.fileUrl">
+			        		<div class="imgmask"></div>
+			        		<div class="price">
+			        			<span>￥{{data.price}}</span>
+			        			<span>￥{{data.marketPrice}}</span>
+			        		</div>
+			        </div> 
+			    </div> 
+			</div>
+	    </div>
+
+	    <div class="relation">
+	    	<h2>好货推荐</h2>
+	    	<div class="relation_nav_box">
+	    		<div class="relation_nav">
+	    			 <div class="swiper-wrapper">
+				        <div class="swiper-slide" v-for="data in rnav">
+				        		<a href=""><span>{{data.categoryName}}</span></a>
+				        </div> 
+			    	</div> 
+	    		</div>
+	    	</div>
+	    	<div class="eachblock">
+	    		<ul>
+	    			<li v-for="data in last">
+	    				<img :src="data.fileUrl" alt="">
+	    				<div class="miaoshu">
+	    					<span></span>
+	    					<span>{{data.brandName}}</span>
+	    					<span>{{data.productName}}</span>
+	    					<span></span>
+	    					<div class="money">
+	    						<span>￥{{data.marketPrice}}</span>
+	    						<span>￥{{data.price}}</span>
+	    						<span>{{data.discount}}折</span>
+	    					</div>
+	    				</div>
+	    			</li>
+	    		</ul>
+	    	</div>
+	    </div>
+
+
 	</div>
 </template>
 
 <script>
 import axios from "axios"
+import Swiper from "swiper"
+import "swiper/dist/css/swiper.css"
 	export default {
 		data () {
 			return {
 				isLong :true,
 				butchange:'更多',
 				brandlist:{},
-				seclist:{}
+				seclist:{},
+				produncts:[],
+				hot:[],
+				rnav:[],
+				last:[]
 			}
 		},
 		
@@ -55,7 +126,7 @@ import axios from "axios"
 			
 		},
 		mounted(){
-			console.log(this.$route.params.logoid)
+			// console.log(this.$route.params.logoid)
 			axios.get(`/appapi/brand/viewCms/v3?logoId=${this.$route.params.logoid}`).then(res=>{
 				// console.log(res.data.body.brandDetail)
 				this.brandlist = res.data.body.brandDetail
@@ -63,11 +134,70 @@ import axios from "axios"
 				
 			})
 			axios.get(`/appapi/brand/product/hotNew/v3?logoId=${this.$route.params.logoid}`).then(res=>{
-				console.log(res.data.body)
+				// console.log(res.data.body)
 				this.seclist = res.data.body
 				
 				
 			})
+			axios.get(`/appapi/brand/product/hotNew/v3?logoId=${this.$route.params.logoid}`).then(res=>{
+				// console.log(res.data.body.newProductTop10)
+				this.produncts = res.data.body.newProductTop10
+				this.$nextTick(()=>{
+					 var swiper = new Swiper('.swiper-container', {
+					      slidesPerView: 3,
+					      spaceBetween: 30,
+					      pagination: {
+					        el: '.swiper-pagination',
+					        clickable: true,
+					      },
+					    });
+
+				})
+
+				
+				
+			})
+			axios.get(`/appapi/brand/product/hotNew/v3?logoId=${this.$route.params.logoid}`).then(res=>{
+				// console.log(res.data.body.hotProductTop10)
+				this.hot = res.data.body.hotProductTop10
+				this.$nextTick(()=>{
+					 var swiper = new Swiper('.swiper-container', {
+					      slidesPerView: 3,
+					      spaceBetween: 30,
+					      pagination: {
+					        el: '.swiper-pagination',
+					        clickable: true,
+					      },
+					    });
+
+				})
+				
+				
+			})
+			axios.get(`/appapi/brand/product/secCategoryProduct/v3?logoId=${this.$route.params.logoid}&pageIndex=1`).then(res=>{
+				console.log(res.data.body.categoryProducts)
+				this.rnav = res.data.body.categories
+				this.last = res.data.body.categoryProducts
+
+				this.$nextTick(()=>{
+					 var swiper = new Swiper('.swiper-container', {
+					      slidesPerView: 3,
+					      spaceBetween: 30,
+					      pagination: {
+					        el: '.swiper-pagination',
+					        clickable: true,
+					      },
+					    });
+
+				})
+				
+				
+			})
+	
+
+
+
+		
 		}
 
 	}	
@@ -176,6 +306,7 @@ import axios from "axios"
 						    color: #d88358;
 						    font-size: 14px;
 						    cursor: pointer;
+						    padding-bottom: 15px;
 						   
 
 						}
@@ -204,5 +335,150 @@ import axios from "axios"
 			}
 			
 		}
+		.brandproducts{
+			h2{
+				font-size: 20px;
+				text-indent: 20px;
+				margin-bottom: 20px;
+			}
+			.swiper-slide{
+				margin:0 15px;
+
+				img{
+					
+					display: inline-block;
+					width:135px;
+					height:180px;
+					
+				}
+				.imgmask{
+					width:135px;
+					height:180px;
+					background-color: rgba(0,0,0,.04);
+					
+					position: absolute;
+					top:0;
+					z-index: 1;
+				}
+				.price{
+					padding-left: 5px;
+					span{
+
+						font-size: 10px;
+					}
+					span:nth-of-type(2){
+						color: #ccc;
+						text-decoration:line-through;
+					}
+				}
+				
+			
+			}
+				
+		}
+		.hotproducts{
+			h2{
+				font-size: 20px;
+				text-indent: 20px;
+				margin-top:40px;
+				margin-bottom: 20px;
+			}
+			.swiper-slide{
+				margin:0 15px;
+
+				img{
+					
+					display: inline-block;
+					width:135px;
+					height:180px;
+					
+				}
+				.imgmask{
+					width:135px;
+					height:180px;
+					background-color: rgba(0,0,0,.04);
+					
+					position: absolute;
+					top:0;
+					z-index: 1;
+				}
+				.price{
+					padding-left: 10px;
+					span{
+						margin-right:5px;
+
+						font-size: 10px;
+					}
+					span:nth-of-type(2){
+						color: #ccc;
+						text-decoration:line-through;
+					}
+				}
+			
+			
+			}
+				
+		}
+		.relation{
+			width:100%;
+			
+			
+			h2{
+
+			}
+			.relation_nav_box{
+				padding-bottom: 15px;
+				border-bottom: 1px solid #ccc;
+				.relation_nav{
+					
+					.swiper-wrapper{
+
+						.swiper-slide{
+							width:30%;
+							a{
+								span{
+
+								}
+							}
+						}
+					}
+				}
+			}
+			.eachblock{
+				
+				ul{
+
+					padding-top: 5px;
+					
+					li{
+						margin-left: 15px;
+						float: left;
+						background: red;
+						height:302px;
+						img{
+							width:165px;
+							height:220px;
+						}
+						.miaoshu{
+							width:165px;
+							span{
+								display: block;
+							}
+							.money{
+								span{
+									float: left;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
+
+
 </style>
+
+	
+
+ 
